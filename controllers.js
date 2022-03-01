f1de1dbf471c87c7bebcae47121ade062f0a8613
Heartless_Caesar <joaovictorcsatu@gmail.com>
2022-02-28 23:23:41 -0300

    Started implementing the search functionality

--- controllers.js
+++ controllers.js
@@ -1,11 +1,27 @@
 const Products = require("./productSchema");
 
 const getAllProductsStatic = async (req, res) => {
-  const allProducts = await Products.find({});
+  const allProducts = await Products.find({ featured: true });
   res.status(201).json({ content: allProducts });
 };
 
-const getAllProducts = async (req, res) => {
+const getProductsQuery = async (req, res) => {
+  const { featured, name, price } = req.query;
+  const queryObject = {};
+
+  if (name) {
+    //IF THE PROVIDED SEARCH NAME EXISTS IT WILL BE FILTERED WITH NO CASE SENSITIVITY
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+
+  if (price) {
+    queryObject.price = price;
+  }
+
+  if (featured) {
+    //IF THE FEATURED PROPERTY OF THE ELEMENT IS TRUE THE QUERYOBJECT PROPERTY WILL BE TRUE IF NOT IT WILL BE SET TO FALSE
+    queryObject.featured = featured === "true" ? true : false;
+  }
   res.status(201).send("Find all test");
 };
 
